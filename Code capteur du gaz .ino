90c7fe967cb2701b8712df813540c55ab7abb51e
Rihem Salah <116517906+rihemsalah@users.noreply.github.com>
2023-01-04 22:30:33 +0100

    Add files via upload

---
+++ Code capteur du gaz .ino
@@ -0,0 +1,1 @@
+#define PIN_MQ2 A1 #define LED 13 int value; void setup() { Serial.begin(9600); pinMode(LED, OUTPUT); pinMode(PIN_MQ2, INPUT); } void loop() { value = analogRead(PIN_MQ2); Serial.println("VALUE - " + String(value)); Serial.println(" "); if (value > 200) { digitalWrite(LED, HIGH); } else { digitalWrite(LED, LOW); } delay(200); }
